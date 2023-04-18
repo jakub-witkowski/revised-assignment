96b556ec37cef0521e33a49989c35da7c2fa6599
Kuba <kuba@MacBook-Air-Kuba.local>
2023-04-18 07:00:14 +0200

    conditional statements removed

--- form-handling.php
+++ form-handling.php
@@ -3,10 +3,10 @@ error_reporting(E_ALL ^ E_NOTICE);
 include 'product-add.php';
 include 'db-connect.php';
 
+$type = strtolower($_POST['type']);
 $sku = $_POST['sku'];
 $name = $_POST['name'];
 $price = $_POST['price'];
-$type = strtolower($_POST['type']);
 $size = $_POST['size'];
 $weight = $_POST['weight'];
 $height = $_POST['height'];
@@ -17,22 +17,11 @@ $result = $db_pdo->query("SELECT COUNT(*) FROM products WHERE sku = '$sku'");
 if ($result->fetchColumn() > 0) {
 	echo '<script>alert("SKU already in use!");</script>';
 } else {
-	if ($type === 'dvd') {
-        $result = "INSERT INTO products (sku, name, price, type, size) VALUES (?,?,?,?,?)";
-        $stmt = $db_pdo->prepare($result);
-        $stmt->execute([$sku, $name, $price, $type, $size]);
+        $result = "INSERT INTO products (type, sku, name, price, size, weight, height, width, length) VALUES (?,?,?,?,?,?,?,?,?)";
         }
-        else if ($type === 'book') {
-        $result = "INSERT INTO products (sku, name, price, type, weight) VALUES (?,?,?,?,?)";
-        $stmt = $db_pdo->prepare($result);
-        $stmt->execute([$sku, $name, $price, $type, $weight]);
-        }
-        else if ($type === 'furniture') {
-        $result = "INSERT INTO products (sku, name, price, type, height, width, length) VALUES (?,?,?,?,?,?,?)";
-        $stmt = $db_pdo->prepare($result);
-        $stmt->execute([$sku, $name, $price, $type, $height, $width, $length]);
-        }
-}
+
+$stmt = $db_pdo->prepare($result);
+$stmt->execute([$type, $sku, $name, $price, $size, $weight, $height, $width, $length]);
 
 if ($stmt->rowCount() == 1) {
     header('Location: index.php?page=product-list'); //
